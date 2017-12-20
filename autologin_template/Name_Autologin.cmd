15a93f4c4d4ea47b866001334d8313de6fb60929
Bene <noplan.pwnz@gmail.com>
2017-12-20 18:39:38 +0100

    autologin-template

---
+++ autologin_template/Name_Autologin.cmd
@@ -0,0 +1,7 @@
+@echo off
+set username=Steam-Username
+reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
+reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
+start steam://open/main
+
+exit /b 0

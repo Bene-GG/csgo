a53a2cbb8dba1922e32ce099fa9777e0c348d370
Bene <noplan.pwnz@gmail.com>
2019-02-08 18:53:40 +0100

    CSGO 1.36.7.6

--- set_ProcessSettings.cmd
+++ set_ProcessSettings.cmd
@@ -6,4 +6,6 @@ echo "Passe Prozesseigenschaften fuer CSGO (Ryzen 1600X) an."
 PowerShell "gps -processname "csgo" | &{ $input.moveNext(); $input.Current.ProcessorAffinity = 4094 }"
 PowerShell "gps -processname "csgo" | &{ $input.moveNext(); $input.Current.PriorityClass = 'AboveNormal' }"
 PowerShell "Set-MpPreference -DisableRealtimeMonitoring $true"
-TASKKILL /F /T /IM NvTelemetryContainer.exe /IM OneDrive.exe /IM acrotray.exe /IM acrobat_sl.exe /IM AdobeUpdateService.exe /IM ctfmon.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe
+TASKKILL /F /T /IM NvTelemetryContainer.exe /IM OneDrive.exe /IM acrotray.exe /IM armsvc.exe /IM smartscreen.exe /IM acrobat_sl.exe /IM AdobeUpdateService.exe /IM ctfmon.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe /IM OfficeClickToRun.exe
+net stop Spooler
+net stop TabletInputService
